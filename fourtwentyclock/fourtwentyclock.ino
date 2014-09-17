0682822a688037434e81b34545127d52edab4904
jciallella <jciallella@gmail.com>
2014-09-17 16:22:38 -0700

    Everything working except for adding push buttons
    Made some major updates, everything working better than before.  Why?
    Not sure exactly!  Seems like a loose USB cable could have been a major
    culprit of earlier troubles (despite having used several different ones)

--- fourtwentyclock/fourtwentyclock.ino
+++ fourtwentyclock/fourtwentyclock.ino
@@ -11,67 +11,48 @@
 
 // Libraries
 #include "Adafruit_LEDBackpack.h"
-#include "Adafruit_NeoMatrix.h"
 #include "Adafruit_NeoPixel.h"
 #include "Adafruit_GFX.h"
 #include "Wire.h"
 #include "RTClib.h"
 #include "Pitches.h"
 
-//// Debouncing Items
+//// Smoothing Variables:
 //#define DEBOUNCE 10                           // # ms (5+ ms is usually plenty)
 //#define NUMBUTTONS sizeof(buttons)            // Macro determines size of array, by checking size
 //byte buttons[] = {2, 3, 4, 5, 12};            // Pins of buttons to be doubounced
 //byte down[NUMBUTTONS], pressed[NUMBUTTONS], released[NUMBUTTONS];
-//
 
-//// Input/Output Pins
-int brightKnob =      A3;      // 7-Sebment
+// Input/Output Pins
+int brightKnob =          A3;      // 7-Sebment
 //int minPlusButton =     2;       // Timekeeping
 //int minMinusButton =    3;       // Timekeeping
 //int hourPlusButton =    4;       // Timekeeping
 //int hourMinusButton =   5;       // Timekeeping
-int brightSwitch = 7;
-int reminderSwitchIn =  6;       // Switch: Reminder
+int reminderSwitch =      6;       // Switch: Reminder
+int brightSwitch =        7;
+int piezoOut =            8;       // Speaker
+int neoPixelPin =         10;      // RGB-LED: Green
 
-int piezoOut =          8;      // Speaker
-//int fourTwentyLED =     9;       // LED: Green
-int neoPixelPin =       10;       // RGB-LED: Green
+// Variables
+int reminderState;                 // Switch (Reminder)
+int noteCounter;                   // Counts theme music notes
+boolean running =        false;    // Colon ON/OFF
+int decimalTime;                   // Overall Clock Time
+long previousMillis;
 
-
-//// Variables
-int reminderState;               // Switch (Reminder)
-int noteCounter;                 // Counts theme music notes
-boolean running =        false;  // Colon ON/OFF
-
-//// Hour Adjustment Variables
+// Hour Adjustment Variables
 //int hourCount;                   // | > For manual time adjustment
 //int minuteCount;                 // | ^
 //int adjustedHour;                // | ^
 //int adjustedMinute;              // | ^
-//
-//
-//// Smoothing Variables:
-//const int numReadings =  12;      // Number of readings to use (speeds/slows)
-//int readings[numReadings];        // Readings from the analog input
-//int index = 0;                    // Index of the current reading
-//int total = 0;                    // Running total
-//int average = 0;                  // Final average
-
-int decimalTime;                 // Overall Clock Time
-long previousMillis;
 
 // Setup Components
 RTC_DS1307 RTC;
 Adafruit_7segment disp = Adafruit_7segment();
 Adafruit_AlphaNum4 alpha4 = Adafruit_AlphaNum4();
-//Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, neoPixelPin, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, neoPixelPin, NEO_GRB + NEO_KHZ800);
 
-// Set Neopixel strip (TRYOUT)
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(5, 8, neoPixelPin,
-NEO_MATRIX_TOP + NEO_MATRIX_RIGHT +
-NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE,
-NEO_GRB + NEO_KHZ800);
 
 // ================================================================================== //
 //                                  *** SETUP ***
@@ -79,21 +60,18 @@ NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
-  pinMode (brightKnob, INPUT);          // Potentiometer
-  pinMode (brightSwitch, INPUT);    // Reminder Switch
-
-  pinMode (reminderSwitchIn, INPUT);    // Reminder Switch
-  pinMode (piezoOut, OUTPUT);         
-
+  pinMode (brightKnob, INPUT);              // Potentiometer
+  pinMode (brightSwitch, INPUT);            // Reminder Switch
+  pinMode (reminderSwitch, INPUT);        // Reminder Switch
 
   //  pinMode (minPlusButton, INPUT);       // Minute +
   //  pinMode (hourPlusButton, INPUT);      // Hour +
   //  pinMode (minMinusButton, INPUT);      // Minute -
   //  pinMode (hourMinusButton, INPUT);     // Hour -
-  //
-  //  pinMode (fourTwentyLED, OUTPUT);      // Green LED
-  pinMode (neoPixelPin, OUTPUT);         // Neopixel
-  //
+  
+  pinMode (piezoOut, OUTPUT);
+  pinMode (neoPixelPin, OUTPUT);            // Neopixel
+  
   //  // Debounce
   //  byte buttonCount;
   //  for (buttonCount = 0; buttonCount < NUMBUTTONS; buttonCount++)
@@ -109,8 +87,8 @@ void setup()
   disp.begin(0x70);                                       // Start 7 Segment
   alpha4.begin(0x71);                                     // Start Alphanumeric
 
-  matrix.begin();                                          // Start Neopixel
-  matrix.show();                                           // Initialize all pixels to 'off'
+  strip.begin();                                          // Start Neopixel
+  strip.show();                                           // Initialize all pixels to 'off'
 }
 
 
@@ -123,19 +101,16 @@ void loop()
   disp.print(getDecimalTime());          // Show 12-Hour Time (7 Segment)
   displayDay();                          // Show Weekday: 14 Segment
   blinkColon();                          // Blink Colon
-  //  smooth();                              // Averages photocell input values
   adjustBrightness();                    // Check Switch & Adjust brightness
   fourTwentyCheck();                     // Check if 4:20pm & Run Alarm
-  //reminderSwitch();                      // LED / Reminder
+  reminderSwitchCheck();                      // LED / Reminder
   //  debounceSwitches();                   // Debounce (check state of press)
- matrix.Color(100, 55, 23);
-      matrix.show();
+
 }
 
 // ================================================================================== //
 //                          *** MAIN CLOCK FUNCTIONS ***
 // ================================================================================== //
-
 
 int getDecimalTime()                                    // Calculate and Adjust Hours
 {
@@ -253,7 +228,7 @@ void adjustBrightness()                                          // Brightness C
   }
   else
   {
-    matrix.setBrightness(neoBrightness);                           // Use Potentiometers to Set Brightness
+    strip.setBrightness(neoBrightness);                           // Use Potentiometers to Set Brightness
   }
 }
 
@@ -263,15 +238,14 @@ void fourTwentyCheck()                                 // 420 Check, Blink & Buz
   int decimalTime = getDecimalTime();
   if (decimalTime == 420)
   {
-    disp.blinkRate(2);
-    disp.print(getDecimalTime());                      // Write time again, so 4:19 isn't displayed
-    disp.writeDisplay();
     fourTwentyWords();
+    disp.print(getDecimalTime());                      // Write time again, so 4:19 isn't displayed
+    disp.blinkRate(2);
+    disp.writeDisplay();
     themeMusic();
   }
   else
   {
-//    analogWrite(fourTwentyLED, 0);
     noTone(piezoOut);
     disp.blinkRate(0);
     alpha4.blinkRate(0);
@@ -281,24 +255,22 @@ void fourTwentyCheck()                                 // 420 Check, Blink & Buz
 
 void fourTwentyWords()                                     // Writes "HIGH" & Blinks Alphanumeric Display
 {
- 
-  char *message = "HIGH  TIME   ";
-    for (uint8_t i=0; i<strlen(message)-4; i++) {
+  char *message = "HIGH  TIME   HIGH  TIME   HIGH  TIME   ";
+  for (uint8_t i = 0; i < strlen(message) - 4; i++) {
     alpha4.writeDigitAscii(0, message[i]);
-    alpha4.writeDigitAscii(1, message[i+1]);
-    alpha4.writeDigitAscii(2, message[i+2]);
-    alpha4.writeDigitAscii(3, message[i+3]);
+    alpha4.writeDigitAscii(1, message[i + 1]);
+    alpha4.writeDigitAscii(2, message[i + 2]);
+    alpha4.writeDigitAscii(3, message[i + 3]);
     alpha4.writeDisplay();
-//    delay(200);
+    delay(200);
+    alpha4.clear();
   }
-// 
-//  alpha4.blinkRate(2);
-//  alpha4.writeDigitAscii(0, 'H');
-//  alpha4.writeDigitAscii(1, 'I');
-//  alpha4.writeDigitAscii(2, 'G');
-//  alpha4.writeDigitAscii(3, 'H');
-//  alpha4.writeDisplay();
-//  alpha4.clear();
+  alpha4.blinkRate(2);
+  alpha4.writeDigitAscii(0, 'H');
+  alpha4.writeDigitAscii(1, 'I');
+  alpha4.writeDigitAscii(2, 'G');
+  alpha4.writeDigitAscii(3, 'H');
+  alpha4.writeDisplay();
 }
 
 
@@ -308,21 +280,21 @@ void fourTwentyWords()                                     // Writes "HIGH" & Bl
 // ================================================================================== //
 
 
-void reminderSwitch()                                   // Checks Switch & Activates LED
+void reminderSwitchCheck()                                   // Checks Switch & Activates LED
 {
-  int reminderState = digitalRead(reminderSwitchIn);        // Check Reminder Switch
+  int reminderState = digitalRead(reminderSwitch);        // Check Reminder Switch
   if (reminderState == 1) rainbowCycle(20);
   else
   {
-    matrix.setBrightness(0);                              // Turning off the shield requires...
-    matrix.show();                                        // ... both of these functions
+    strip.setBrightness(0);                              // Turning off the shield requires...
+    strip.show();                                        // ... both of these functions
   }
 }
 
 
 void beep(int note, int duration)                        // Creates Individual Notes & Alternates LED's
 {
-  long interval = 50;
+  long interval = 20;
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis > interval)
   {
@@ -331,13 +303,23 @@ void beep(int note, int duration)                        // Creates Individual N
     tone(piezoOut, note, duration);                          //Play tone on buzzerPin
     if (noteCounter % 2 == 0)                                // Alternate Green then UV LED's
     {
-    // Do something fun with neopixel here
-    delay(duration);
+      uint16_t j;
+      for (j = 0; j < 40 ; j++)
+      {
+        strip.setPixelColor(j, 189, 43, 215);    // Purple Color
+        strip.show();
+      }
+      delay(duration);
     }
     else
     {
-    // Do something fun with neopixel here
-    delay(duration);
+      uint16_t j;
+      for (j = 0; j < 40 ; j++)
+      {
+        strip.setPixelColor(j, 143, 230, 41);    // Green Color
+        strip.show();
+      }
+      delay(duration);
     }
     noTone(piezoOut);
     noteCounter++;
@@ -345,60 +327,46 @@ void beep(int note, int duration)                        // Creates Individual N
 }
 
 
-void rainbowCycle(uint8_t wait)                             // Runs rainbow led setup
+void rainbowCycle(uint8_t wait)                             // Runs rainbow led fade
 {
   uint16_t i, j;
 
-  for (j = 0; j < 256 ; j++)                                 // *1 cycles of all colors on wheel
+  for (j = 0; j < 256 ; j++)                                 
   {
-    for (i = 0; i < matrix.numPixels(); i++)
+    for (i = 0; i < strip.numPixels(); i++)
     {
-      matrix.setPixelColor(i, Wheel(((i * 256 / matrix.numPixels()) + j) & 255));
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
     }
-    matrix.show();
+    strip.show();
     delay(wait);
   }
 }
 
-// Required for rainbowCycle()
+                                                              // Required for rainbowCycle()
 uint32_t Wheel(byte WheelPos)                                 // Colors transition r - g - b - r - g...
 {
   if (WheelPos < 85)
   {
-    return matrix.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
   }
   else if (WheelPos < 170)
   {
     WheelPos -= 85;
-    return matrix.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   }
   else
   {
     WheelPos -= 170;
-    return matrix.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
 
 
-//// ================================================================================== //
-////                             *** OTHER FUNCTIONS ***
-//// ================================================================================== //
-//
-//
-//void smooth()                                         // Averages photocell readings
-//{
-//  total = total - readings[index];                    // subtract the last reading
-//  readings[index] = analogRead(photoCellIn);          // read from the sensor
-//  total = total + readings[index];                    // add the reading to the total
-//  index = index + 1;                                  // advance to next position in array
-//  if (index >= numReadings) index = 0;                // if we're at the end of the array...
-//  average = total / numReadings;
-//
-//  autoBrightAverage = average;
-//
-//  delay(1);                                           // stability delay in between reads
-//}
-//
+// ================================================================================== //
+//                             *** OTHER FUNCTIONS ***
+// ================================================================================== //
+
+
 //void debounceSwitches()
 //{
 //  for (byte i = 0; i < NUMBUTTONS; i++)
